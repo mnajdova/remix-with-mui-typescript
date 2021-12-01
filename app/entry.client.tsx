4cf14dcb0b0b5ffff6c47a944b750f66094bc4ca
Marija Najdova <mnajdova@gmail.com>
2021-12-01 09:17:48 +0100

    add MUI and make SSR work

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,20 @@
 import { hydrate } from "react-dom";
 import { RemixBrowser } from "remix";
+import { CacheProvider, ThemeProvider } from "@emotion/react";
+import CssBaseline from "@mui/material/CssBaseline";
 
-hydrate(<RemixBrowser />, document);
+import createEmotionCache from "./src/createEmotionCache";
+import theme from "./src/theme";
+
+const emotionCache = createEmotionCache();
+
+hydrate(
+  <CacheProvider value={emotionCache}>
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <RemixBrowser />
+    </ThemeProvider>
+  </CacheProvider>,
+  document
+);
